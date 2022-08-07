1e814aa0ea6175c9f6334b1afaee306b4b3e40d5
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2022-08-07 13:38:17 -0700

    feat: should not create Post without content

--- src/main/java/dev/emmanoel/ignitefeedbackend/domain/Post.java
+++ src/main/java/dev/emmanoel/ignitefeedbackend/domain/Post.java
@@ -4,12 +4,18 @@ import java.util.Objects;
 
 public class Post {
     private final User author;
+    private final Content content;
 
-    public Post(final User author) {
+    public Post(final User author, final Content content) {
         this.author = Objects.requireNonNull(author);
+        this.content = Objects.requireNonNull(content);
     }
 
     public User getAuthor() {
         return author;
+    }
+
+    public Content getContent() {
+        return content;
     }
 }
