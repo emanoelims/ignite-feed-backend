12e37aa33b27075d9addeb546726369379e2c2c6
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2022-08-07 14:02:24 -0700

    test: should not create Post without publication date

--- src/test/java/dev/emmanoel/ignitefeedbackend/domain/PostTest.java
+++ src/test/java/dev/emmanoel/ignitefeedbackend/domain/PostTest.java
@@ -3,6 +3,8 @@ package dev.emmanoel.ignitefeedbackend.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PostTest {
@@ -11,13 +13,23 @@ class PostTest {
     @DisplayName("should not create post without author")
     void shouldNotCreatePostWithoutAuthor() {
         var content = new Content();
-        assertThrows(NullPointerException.class, () -> new Post(null, content));
+        var publicationDate = LocalDateTime.now();
+        assertThrows(NullPointerException.class, () -> new Post(null, content, publicationDate));
     }
 
     @Test
     @DisplayName("should not create post without content")
     void shouldNotCreatePostWithoutContent() {
         var author = new User();
-        assertThrows(NullPointerException.class, () -> new Post(author, null));
+        var publicationDate = LocalDateTime.now();
+        assertThrows(NullPointerException.class, () -> new Post(author, null, publicationDate));
+    }
+
+    @Test
+    @DisplayName("should not create post without publication date")
+    void shouldNotCreatePostWithoutPublicationDate() {
+        var author = new User();
+        var content = new Content();
+        assertThrows(NullPointerException.class, () -> new Post(author, content, null));
     }
 }
