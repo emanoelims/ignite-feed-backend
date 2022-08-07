e0554f9cfb064059737c4389dff7195e747d4427
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2022-08-07 14:29:19 -0700

    feat: should not create unnamed User

--- src/main/java/dev/emmanoel/ignitefeedbackend/domain/User.java
+++ src/main/java/dev/emmanoel/ignitefeedbackend/domain/User.java
@@ -1,4 +1,15 @@
 package dev.emmanoel.ignitefeedbackend.domain;
 
+import java.util.Objects;
+
 public class User {
+    private final String name;
+
+    public User(final String name) {
+        this.name = Objects.requireNonNull(name);
+    }
+
+    public String getName() {
+        return name;
+    }
 }
