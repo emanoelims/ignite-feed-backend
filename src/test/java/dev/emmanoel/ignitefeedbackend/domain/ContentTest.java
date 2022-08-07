bd0afff6626075e42db4d49132125ab2c0fea7de
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2022-08-07 15:43:02 -0700

    test: should add more than one paragraph

--- src/test/java/dev/emmanoel/ignitefeedbackend/domain/ContentTest.java
+++ src/test/java/dev/emmanoel/ignitefeedbackend/domain/ContentTest.java
@@ -21,4 +21,13 @@ class ContentTest {
         content.addParagraph("Fala pessoal, tudo joia?");
         assertEquals(1, content.paragraphCount());
     }
+
+    @Test
+    @DisplayName("should add more than one paragraph")
+    void shouldAddMoreThanOneParagraph() {
+        var content = new Content();
+        content.addParagraph("Fala pessoal, tudo joia?");
+        content.addParagraph("Hoje vamos aprender mais sobre DDD e Clean Arch");
+        assertEquals(2, content.paragraphCount());
+    }
 }
