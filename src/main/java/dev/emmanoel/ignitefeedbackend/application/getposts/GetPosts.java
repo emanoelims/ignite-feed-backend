9b6a6e2a6874d0a4d7bf6c4f7334297b718d3c93
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2022-08-08 17:56:08 -0700

    feat: should return list of posts

---
+++ src/main/java/dev/emmanoel/ignitefeedbackend/application/getposts/GetPosts.java
@@ -0,0 +1,27 @@
+package dev.emmanoel.ignitefeedbackend.application.getposts;
+
+import dev.emmanoel.ignitefeedbackend.domain.post.PostRepository;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class GetPosts {
+    private final PostRepository postRepo;
+
+    public GetPosts(PostRepository postRepository) {
+        postRepo = postRepository;
+    }
+
+    public List<GetPostsOutput> execute() {
+        return postRepo.findAll().stream().map(post -> {
+            var author = new AuthorOutput(post.getAuthor().getName(), post.getAuthor().getRole());
+            var content = post.getContent()
+                .getContents()
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(e -> e.getKey().toString(), Map.Entry::getValue));
+            return new GetPostsOutput(author, content, post.getPublicationDate());
+        }).toList();
+    }
+}
