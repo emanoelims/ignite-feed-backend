edbef07310435f49eb952625c43348cc998c8bf0
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2022-08-08 17:56:17 -0700

    test: should return list of posts

---
+++ src/test/java/dev/emmanoel/ignitefeedbackend/application/GetPostsTest.java
@@ -0,0 +1,30 @@
+package dev.emmanoel.ignitefeedbackend.application;
+
+import dev.emmanoel.ignitefeedbackend.application.getposts.GetPosts;
+import dev.emmanoel.ignitefeedbackend.domain.post.Content;
+import dev.emmanoel.ignitefeedbackend.domain.post.Post;
+import dev.emmanoel.ignitefeedbackend.domain.post.PostRepository;
+import dev.emmanoel.ignitefeedbackend.domain.user.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class GetPostsTest {
+
+    @Test
+    @DisplayName("should return list of posts")
+    void shouldReturnListOfPosts() {
+        var post = new Post(new User("Emmanoel", "CEO @MendesAssessoria"), new Content(), LocalDateTime.now());
+        var postRepo = mock(PostRepository.class);
+        when(postRepo.findAll()).thenReturn(List.of(post, post));
+        var sut = new GetPosts(postRepo);
+        var posts = sut.execute();
+        assertEquals(2, posts.size());
+    }
+}
