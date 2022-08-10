643c6bad08802ece20996d44ebf5be1c047d6ee5
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2022-08-09 20:12:14 -0700

    feat: should create a post

---
+++ src/main/java/dev/emmanoel/ignitefeedbackend/application/createpost/CreatePostOutput.java
@@ -0,0 +1,6 @@
+package dev.emmanoel.ignitefeedbackend.application.createpost;
+
+import java.util.UUID;
+
+public record CreatePostOutput(UUID postId) {
+}
---
+++ src/main/java/dev/emmanoel/ignitefeedbackend/domain/user/UserNotFoundException.java
@@ -0,0 +1,4 @@
+package dev.emmanoel.ignitefeedbackend.domain.user;
+
+public class UserNotFoundException extends RuntimeException {
+}
